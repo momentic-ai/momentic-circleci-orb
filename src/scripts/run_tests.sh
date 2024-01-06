4ff27f72391e8f56d4de69281e592679ae4ecb32
wuweiweiwu <wei-wei@momentic.ai>
2024-01-05 20:58:23 -0800

    exit codes

--- src/scripts/run_tests.sh
+++ src/scripts/run_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e 
+set -o pipefail
+
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TESTS=$(circleci env subst "${PARAM_TESTS}")
 API_KEY=$(circleci env subst "${PARAM_API_KEY}")
