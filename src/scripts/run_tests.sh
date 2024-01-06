2ea0e82fb6a17f5f1d514d1a70050eeeaf07bef1
wuweiweiwu <wei-wei@momentic.ai>
2024-01-05 21:05:21 -0800

    try setting variable explicitely

--- src/scripts/run_tests.sh
+++ src/scripts/run_tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e 
-set -o pipefail
-
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TESTS=$(circleci env subst "${PARAM_TESTS}")
 API_KEY=$(circleci env subst "${PARAM_API_KEY}")
