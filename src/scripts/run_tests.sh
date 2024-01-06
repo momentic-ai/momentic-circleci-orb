799afecfa6d86f4f8e181323d9a0d0061c713726
wuweiweiwu <wei-wei@momentic.ai>
2024-01-05 20:32:31 -0800

    correct usage of parameters and variables

--- src/scripts/run_tests.sh
+++ src/scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TESTS=$(circleci env subst "${PARAM_TESTS}")
-MOMENTIC_API_KEY=$(circleci env subst "${PARAM_MOMENTIC_API_KEY}")
+API_KEY=$(circleci env subst "${PARAM_API_KEY}")
 
-echo "Running tests: ${TESTS} ${MOMENTIC_API_KEY}"
+echo "Running tests: ${TESTS} ${API_KEY}"
