3a49f8693f88098e54dae9540c82b15ff4ddb867
wuweiweiwu <wei-wei@momentic.ai>
2024-01-10 13:30:16 -0800

    trigger a CI run

--- src/scripts/run_tests.sh
+++ src/scripts/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TESTS=$(circleci env subst "${PARAM_TESTS}")
 API_KEY=$(circleci env subst "${PARAM_API_KEY}")
